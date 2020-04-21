1d7098d4c6fa823c5fd21316672df56c8b1b9928
praveen7101994 <praveenpatelonline@gmail.com>
2020-04-21 16:13:11 +0530

    Basic routes setup with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/Navbar.component';
 import Footer from './components/layout/Footer.component';
 import Landing from './components/layout/Landing.component';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
